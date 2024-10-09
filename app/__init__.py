ef289ae485f4e48061e613b11741e32ce0c07e95
Steve Copley <steve.copley@waimea.school.nz>
2024-10-09 22:30:53 +1300

    Comment and code formatting

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-'''
+"""
 MAIN APP ROUTES
-'''
+"""
 
 from flask import Flask
 from flask import render_template
@@ -9,23 +9,19 @@ from flask import render_template
 app = Flask(__name__)
 
 
-#-------------------------------------------------------
-@app.get('/')
-
+# -------------------------------------------------------
+@app.get("/")
 def hello():
-    '''
-    Home page
-    '''
-    return render_template('pages/home.jinja')
+    """
+    Home Page
+    """
+    return render_template("pages/home.jinja")
 
 
-#-------------------------------------------------------
-@app.get('/about')
-
+# -------------------------------------------------------
+@app.get("/about")
 def about():
-    '''
+    """
     About page
-    '''
-    return render_template('pages/about.jinja')
-
-
+    """
+    return render_template("pages/about.jinja")
